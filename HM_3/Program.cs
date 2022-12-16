850a638b1f082af7789bbf1e4b25f8faaa8fd057
Laska <klimovskih.laska@mail.ru>
2022-12-16 20:38:28 +0700

    дз на проверку

---
+++ HM_3/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("введите количество строк");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[linesVol, columnsVol];
+
+int[,] matrixA = new int[linesVol, columnsVol];
+int[,] matrixB = new int[linesVol, columnsVol];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+
+Console.WriteLine();
+Console.WriteLine("первый массив ");
+PrintArray(matrixA);
+Console.WriteLine("второй массив ");
+PrintArray(matrixB);
+
+  int[,] matrixC = new int[linesVol, columnsVol];
+        for (int i = 0; i < matrixA.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixA.GetLength(1); j++)
+            {
+              for (int k = 0; k < matrixA.GetLength(0); k++)
+              {
+               
+                    matrixC[i, j] =  matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+                
+              }
+            }
+        }
+
+Console.WriteLine(" массив ");
+PrintArray(matrixC);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
